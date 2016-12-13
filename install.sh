39ffdd0462e84c56bd6c76887486f4d647a0b047
Michail Kabanov <mcdba@mail.ru>
2016-12-13 11:08:22 +0300

    up

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
-sudo apt-get update
+#!/bin/bash
+#Filename: install.sh
+#Description: Install sysinfo
 
+# sudo apt-get update
 # sudo apt-get -y upgrade 
-sudo apt-get -y install git mc 
-#git clone https://github.com/mcdba/sysinfo.git
+
+# sudo apt-get -y install git mc 
+# git clone https://github.com/mcdba/sysinfo.git
+
+
 
 
 sudo apt-get -y install apache2 php 
@@ -13,7 +19,7 @@ sudo apt-get -y install mysql-server
 sudo apt-get -y install php-mysql
 
 sudo rm /etc/apache2/sites-enabled/*
-sudo ln -s config/sysinfo /etc/apache2/sites-enabled/sysinfo
+sudo cp  config/sysinfo /etc/apache2/sites-enabled/sysinfo
 
 sudo rm /etc/apache2/ports.conf
 sudo cp config/ports.conf /etc/apache2/ports.conf
@@ -23,4 +29,6 @@ sudo service apache2 restart
 
 sudo apt-get -y install nginx
 sudo rm /etc/nginx/sites-enabled/*
-sudo ln -s config/sysinfo_nginx /etc/nginx/sites-enabled/sysinfo_nginx
+sudo cp config/sysinfo_nginx /etc/nginx/sites-enabled/sysinfo_nginx
+
+sudo service nginx restart
