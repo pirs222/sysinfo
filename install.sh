06039f53733c6fc2b24b0b2b93e8532963c35249
Michail Kabanov <mcdba@mail.ru>
2016-12-13 11:52:31 +0300

    add blank cron

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Filename: install.sh
 #Description: Install sysinfo
-
+MYSQL_PASSWORD="mysqlpass"
 sudo apt-get update
 # sudo apt-get -y upgrade 
 
@@ -13,8 +13,8 @@ sudo apt-get update
 
 sudo apt-get -y install apache2 php 
 sudo apt-get -y install libapache2-mod-php php-mysql
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysqlpass'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysqlpass'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password $MYSQL_PASSWORD'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD'
 sudo apt-get -y install mysql-server
 sudo apt-get -y install php-mysql
 
@@ -32,3 +32,11 @@ sudo rm /etc/nginx/sites-enabled/*
 sudo cp config/sysinfo_nginx /etc/nginx/sites-enabled/sysinfo_nginx
 
 sudo service nginx restart
+
+
+# cron
+chmod +x $PWD/config/cron_procedure.sh
+
+crontab -l > $PWD/mycron.sh
+echo "0-59 * * * * $PWD/config/cron_procedure.sh" >> $PWD/mycron.sh
+crontab $PWD/mycron.sh
