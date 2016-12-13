f71dc8a65c1887155e590d35b2ebe19af0d65afb
Michail Kabanov <mcdba@mail.ru>
2016-12-13 10:33:38 +0300

    adding nginx

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 sudo apt-get update
+
 # sudo apt-get -y upgrade 
+sudo apt-get -y install git mc 
+#git clone https://github.com/mcdba/sysinfo.git
+
+
 sudo apt-get -y install apache2 php 
 sudo apt-get -y install libapache2-mod-php php-mysql
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysqlpass'
@@ -11,6 +16,11 @@ sudo rm /etc/apache2/sites-enabled/*
 sudo ln -s config/sysinfo /etc/apache2/sites-enabled/sysinfo
 
 sudo rm /etc/apache2/ports.conf
-sudo copy config/ports.conf /etc/apache2/ports.conf
+sudo cp config/ports.conf /etc/apache2/ports.conf
+sudo cp config/index.php /var/www/html/index.php
 
 sudo service apache2 restart
+
+sudo apt-get -y install nginx
+sudo rm /etc/nginx/sites-enabled/*
+sudo ln -s config/sysinfo_nginx /etc/nginx/sites-enabled/sysinfo_nginx
