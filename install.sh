07495b4883a64a8136a8f34e4b94f7d29a3be63a
Michail Kabanov <mcdba@mail.ru>
2016-12-14 14:25:03 +0300

    wget adminer + upgrade

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ DB_USER='root';
 DB_PASSWD='mysqlpass';
 
 sudo apt-get update
-# sudo apt-get -y upgrade 
+sudo apt-get -y upgrade 
 
 # sudo apt-get -y install git mc 
 # git clone https://github.com/mcdba/sysinfo.git
@@ -14,7 +14,7 @@ sudo apt-get update
 
 
 sudo apt-get -y install apache2 php 
-sudo apt-get -y install libapache2-mod-php php-mysql
+sudo apt-get -y install libapache2-mod-php
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password $DB_PASSWD'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $DB_PASSWD'
 sudo apt-get -y install mysql-server
@@ -26,6 +26,8 @@ sudo ln -s  $PWD/config/sysinfo_apache /etc/apache2/sites-enabled/sysinfo_apache
 sudo rm /etc/apache2/ports.conf
 sudo cp $PWD/config/ports.conf /etc/apache2/ports.conf
 sudo ln -s $PWD/config/index.php /var/www/html/index.php
+sudo wget -O /var/www/html/adminer.php https://github.com/vrana/adminer/releases/download/v4.2.5/adminer-4.2.5-en.php 
+
 
 sudo service apache2 restart
 
