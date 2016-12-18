45c25176cf732abfb2b4f42eb96c8e8d8c5e583e
lostmap <lostmap4@gmail.com>
2016-12-19 01:39:42 +0300

    Update log_data.sh

--- config/log_data.sh
+++ config/log_data.sh
@@ -31,6 +31,6 @@ write_mysql "tcpdump Top Talkers src/dst ip, port, bps" "$(sudo tcpdump -nn -r $
 # delete old tcpdump files
 rm -f $filename
 write_mysql "Listening socket" "$(netstat -nltp)";
-write_mysql "CPU load" "$(top|head -3|awk 'NR >2 {gsub(",",".",$0);printf("%8.1f %%us %8.1f %%sys %8.1f %%id %8.1f %%iowait \n", $2+$6,$4,$8,$10)}')";
+write_mysql "CPU load" "$(mpstat | awk 'NR >3{gsub(",",".",$0);printf("%8.1f %%us %8.1f %%sys %8.1f %%idle %8.1f %%iowait \n", $3+$4,$5,$12,$6)}')";
 write_mysql "Disk Usage" "$(df --output=source,pcent,ipcent,target|grep -v -E '% /dev|% /proc|% /sys'|awk '{	p=int(substr($2,1,length($2)-1));	i=int(substr($3,1,length($3)-1)); if(i>p) p=i; if( p>=90) print "<span class=\"crit\">"$0"</span>"; else if(p>=80) print "<span class=\"warn\">"$0"</span>"; else print $0;}')";
 
