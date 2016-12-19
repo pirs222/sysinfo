382fac57b8e45c0945f4d2541f1a2dba06e46f3c
lostmap <lostmap4@gmail.com>
2016-12-19 15:43:00 +0400

    Update sysinfo.php

--- config/sysinfo.php
+++ config/sysinfo.php
@@ -22,6 +22,19 @@ echo "Nginx port=".$_SERVER['SERVER_PORT']."<br>";
 echo "HTTP_X_NGX_VERSION=".$_SERVER['HTTP_X_NGX_VERSION']."<br>";
 //echo var_dump($_SERVER);
 
+//65 сек		
+ $resl = $mysqli->query("SELECT  id,date FROM log ORDER BY id DESC LIMIT 1;");
+    $rowl = $resl->fetch_assoc();
+    $date_last=strtotime($rowl['date']);
+    $date_current=time();
+    $date_diff=$date_current-$date_last;
+    if ($date_diff>65){
+			$header_class="fail";
+    }else{
+			$header_class="ok";
+    };
+//проверка
+		
 // Соединяемся, выбираем базу данных
 	$mysqli = new mysqli('localhost', 'root', 'mysqlpass',"sysinfo");
 
